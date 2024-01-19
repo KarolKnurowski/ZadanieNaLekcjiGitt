c53f525eddcd765220e34120bfdf109876e7c694
Karol Knurowski <knurowskikarol12345@gmail.com>
2024-01-19 11:34:03 +0100

    Zmieniono Bazę Danych DataBase oraz dodano usera

--- ZadanieNaLekcjiGit/ZadanieNaLekcjiGit/DataBase.cs
+++ ZadanieNaLekcjiGit/ZadanieNaLekcjiGit/DataBase.cs
@@ -30,6 +30,30 @@ namespace ZadanieNaLekcjiGit
         {
             return _database.QueryAsync<User>("SELECT * FROM User");
         }
+        public Task<List<Subject>> GetSubjects()
+        {
+            return _database.QueryAsync<Subject>("SELECT * FROM Subject");
+        }
+
+        public Task<List<Score>> GetScories()
+        {
+            return _database.QueryAsync<Score>("SELECT * FROM Score");
+        }
+
+        public Task<int> InsertSubject(Subject subject)
+        {
+            return _database.InsertAsync(subject);
+        }
+
+        public Task<int> InsertScore(Score score)
+        {
+            return _database.InsertAsync(score);
+        }
+
+        public Task<List<Score>> GetScories(int user_id, int subject_id, string period)
+        {
+            return _database.QueryAsync<Score>("SELECT * FROM Score WHERE User_id=? AND Subject_id=? AND Period=?", user_id, subject_id, period);
+        }
 
     }
 }
--- ZadanieNaLekcjiGit/ZadanieNaLekcjiGit/Login.xaml.cs
+++ ZadanieNaLekcjiGit/ZadanieNaLekcjiGit/Login.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZadanieNaLekcjiGit.tabele;
 
 namespace ZadanieNaLekcjiGit
 {
@@ -15,6 +16,35 @@ namespace ZadanieNaLekcjiGit
         public Login()
         {
             InitializeComponent();
+            dodaj();
+        }
+        public async void dodaj()
+        {
+            User x = new User()
+            {
+                Name = "Karol",
+                Surname = "Knurowski",
+                Login = "000001n",
+                Password = "karol123",
+                IsTeacher = true
+            };
+            await App.Database.DodajUsera(x);
+            Subject sbj = new Subject()
+            {
+                Name = "Programowanie"
+            };
+            await App.Database.InsertSubject(sbj);
+            Score s = new Score()
+            {
+                User_id = 1,
+                Subject_id = 1,
+                Subject_name = "Programowanie",
+                Value = "5+",
+                Date = DateTime.Now,
+                Description = "Sprawdzian",
+                Period = "Okres 1"
+            };
+            await App.Database.InsertScore(s);
         }
 
         private async void Zaloguj_Clicked(object sender, EventArgs e)
