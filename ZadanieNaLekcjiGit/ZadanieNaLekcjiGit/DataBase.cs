9fc68ac80952ffb3b8ea62b1fbeb301c821b8658
Karol Knurowski <knurowskikarol12345@gmail.com>
2024-01-19 10:56:27 +0100

    Zmodyfikowano klasę DataBase

--- ZadanieNaLekcjiGit/ZadanieNaLekcjiGit/DataBase.cs
+++ ZadanieNaLekcjiGit/ZadanieNaLekcjiGit/DataBase.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using ZadanieNaLekcjiGit.tabele;
 
 namespace ZadanieNaLekcjiGit
 {
     public class DataBase
     {
+        readonly SQLiteAsyncConnection _database;
+        public DataBase(string db)
+        {
+            _database = new SQLiteAsyncConnection(db);
+            _database.CreateTableAsync<User>().Wait();
+            _database.CreateTableAsync<Score>().Wait();
+            _database.CreateTableAsync<Subject>().Wait();
+        }
+
+        public Task<int> DodajUsera(User user)
+        {
+            return _database.InsertAsync(user);
+        }
+
+        public Task<List<User>> WezUsera()
+        {
+            return _database.QueryAsync<User>("SELECT * FROM User");
+        }
+
     }
 }
