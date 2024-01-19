a3aba03db3b6886a0061482997378cc3f3a96ab0
Karol Knurowski <knurowskikarol12345@gmail.com>
2024-01-19 10:50:26 +0100

    Zmodyfikowano Tabele User

--- ZadanieNaLekcjiGit/ZadanieNaLekcjiGit/tabele/User.cs
+++ ZadanieNaLekcjiGit/ZadanieNaLekcjiGit/tabele/User.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace ZadanieNaLekcjiGit.tabele
 {
     public class User
     {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Surname {  get; set; }
+        public string Login {  get; set; }
+        public string Password { get; set; }
+        public bool IsTeacher { get; set; }
+
     }
 }
